f5547d5c2a1a98e77a07d6b5e3e9e1338db1120f
lukeferenc <luke.theduke.of.cornwall@gmail.com>
2021-05-05 17:20:04 +0200

    1.2 removed food, added pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
-alert("Hello World")
-
-let favouriteFood = "Meat";
-document.write(favouriteFood);
-favouriteFood = "Steak";
-document.write(favouriteFood);
-
+let pokemonList = [
+    {number: 149, name: 'Dragonite', color: 'Orange', height: 2.2, typing: [dragon,flying] },
+    {number: 131, name: 'Lapras', color: 'blue', height: 2.5, typing: [water,ice] },
+    {number: 112, name: 'Rhydon', color: 'grey', height: 1.9, typing: [rock,ground] },
+]

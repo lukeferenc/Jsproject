9aab936ab82df6a8824b52b177bf7afe32cb3c41
lukeferenc <luke.theduke.of.cornwall@gmail.com>
2021-05-07 17:06:45 +0200

    1.2 var fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-    {number: 149, name: 'Dragonite', color: 'Orange', height: 2.2, typing: [dragon,flying] },
-    {number: 131, name: 'Lapras', color: 'blue', height: 2.5, typing: [water,ice] },
-    {number: 112, name: 'Rhydon', color: 'grey', height: 1.9, typing: [rock,ground] },
+    {number: 149, name: 'Dragonite', color: 'Orange', height: 2.2, typing: ["dragon","flying"] },
+    {number: 131, name: 'Lapras', color: 'blue', height: 2.5, typing: ["water","ice"] },
+    {number: 112, name: 'Rhydon', color: 'grey', height: 1.9, typing: ["rock","ground"] },
 ]

ca71a3d36e7aece003a530141aa3e1bcfa837a3b
lukeferenc <luke.theduke.of.cornwall@gmail.com>
2021-05-09 21:40:45 +0200

    1.3 loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
+
 let pokemonList = [
     {number: 149, name: 'Dragonite', color: 'Orange', height: 2.2, typing: ["dragon","flying"] },
     {number: 131, name: 'Lapras', color: 'blue', height: 2.5, typing: ["water","ice"] },
     {number: 112, name: 'Rhydon', color: 'grey', height: 1.9, typing: ["rock","ground"] },
 ]
+
+for (let i = 1; i < pokemonList.height; i++{
+    if (pokemonList[i].height <1)
+    document.write(pokemonList[i].name + " is a small pokemon ");
+  }else if (pokemonList[i].typing = dragon) {
+    document.write(pokemonList[i].name + " is a dragon ");
+  }else if (pokemonList[i].typing = flying) {
+    document.write(pokemonList[i].name + " can fly ");
+}
+
+/* where does the document,write go? */

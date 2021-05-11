a3e2fa64787e2e2ea2238c00453a67a8dd76c5a7
lukeferenc <luke.theduke.of.cornwall@gmail.com>
2021-05-11 13:33:37 +0200

    1.3 looping

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
 
 let pokemonList = [
-    {number: 149, name: 'Dragonite', color: 'Orange', height: 2.2, typing: ["dragon","flying"] },
-    {number: 131, name: 'Lapras', color: 'blue', height: 2.5, typing: ["water","ice"] },
+    {number: 19, name: 'Rattata', color: 'purple', height: 0.3, typing: ["normal"] },
     {number: 112, name: 'Rhydon', color: 'grey', height: 1.9, typing: ["rock","ground"] },
+    {number: 131, name: 'Lapras', color: 'blue', height: 2.5, typing: ["water","ice"] },
+    {number: 149, name: 'Dragonite', color: 'Orange', height: 2.2, typing: ["dragon","flying"] },
 ]
 
-for (let i = 1; i < pokemonList.height; i++) {
-    if (pokemonList[i].height < 1)
-    document.write(pokemonList[i].name + " is a small pokemon ");
-  }else if (pokemonList[i].typing = dragon) {
-    document.write(pokemonList[i].name + " is a dragon ");
-  }else if (pokemonList[i].typing = flying) {
-    document.write(pokemonList[i].name + " can fly ");
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 2) {
+    document.write(pokemonList[i].name + " is a big pokemon! <br/>");
+  } else if (pokemonList[i].height < 0.5 ) {
+    document.write(pokemonList[i].name + "is a tiny pokemon! <br/>");
+  } if (pokemonList[i].typing.includes("dragon") ){
+    document.write(pokemonList[i].name + " is a dragon! <br/>");
+  } if (pokemonList[i].typing.includes("flying") ){
+    document.write(pokemonList[i].name + " can fly! <br/>");
+  }
 }
-

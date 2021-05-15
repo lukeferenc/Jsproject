277c3d9644738e30ce7966a7ba5bdbf70ae77996
lukeferenc <luke.theduke.of.cornwall@gmail.com>
2021-05-15 14:57:23 +0200

    1.5 adding ForEach loops and IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,33 @@
-
-let pokemonList = [
-    {number: 19, name: 'Rattata', color: 'purple', height: 0.3, typing: ["normal"] },
-    {number: 112, name: 'Rhydon', color: 'grey', height: 1.9, typing: ["rock","ground"] },
-    {number: 131, name: 'Lapras', color: 'blue', height: 2.5, typing: ["water","ice"] },
-    {number: 149, name: 'Dragonite', color: 'Orange', height: 2.2, typing: ["dragon","flying"] },
-]
-
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 2) {
-    document.write(pokemonList[i].name + " is a big pokemon! <br/>");
-  } else if (pokemonList[i].height < 0.5 ) {
-    document.write(pokemonList[i].name + " is a tiny pokemon! <br/>");
-  } if (pokemonList[i].typing.includes("dragon") ){
-    document.write(pokemonList[i].name + " is a dragon! <br/>");
-  } if (pokemonList[i].typing.includes("flying") ){
-    document.write(pokemonList[i].name + " can fly! <br/>");
+let pokemonRepository = (function () {
+  let pokemonList = [
+      {number: 19, name: 'Rattata', color: 'purple', height: 0.3, typing: ["normal"] },
+      {number: 112, name: 'Rhydon', color: 'grey', height: 1.9, typing: ["rock","ground"] },
+      {number: 131, name: 'Lapras', color: 'blue', height: 2.5, typing: ["water","ice"] },
+      {number: 149, name: 'Dragonite', color: 'Orange', height: 2.2, typing: ["dragon","flying"] },
+  ]
+  
+  pokemonList.forEach(function(myLoopFunction) {
+      if (pokemon.height > 2) {
+        document.write(pokemon.name + "is a big pokemon <br/>");
+    } else if (pokemon.height < 0.5 ) {
+        document.write(pokemon.name + "is a tiny pokemon! <br/>")
+    } if (pokemon.typing.includes("dragon")) {
+        document.write(pokemon.name + "is a dragon <br/>")
+    } if (pokemon.typing.includes("flying")) {
+        document.write(pokemon.name + "can fly <br/>")
+    }
   }
-}
+  
+function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+  
+function getAll() {
+      return pokemonList;
+    }
+  
+return {
+      add: add,
+      getAll: getAll
+    };
+  })();
